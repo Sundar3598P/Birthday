af9b2c7b2957356491aba5063cff013173f63fd9
Sundar3598P <sundar3598.p@gmail.com>
2021-12-09 17:54:32 +0530

    React

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        id: 1,
+        name: 'Sundar',
+        age:24,
+        image: 'https://res.cloudinary.com/sundar/image/upload/v1612838556/WhatsApp_Image_2021-02-09_at_8.10.15_AM_s8eajj.jpg'
+    },
+
+    {
+        id: 2,
+        name: 'Mahesh',
+        age:46,
+        image: 'https://res.cloudinary.com/sundar/image/upload/v1626358389/mahesh_babu_img_y12chn.jpg'
+    },
+
+    {
+        id: 3,
+        name: 'Arjun',
+        age:38,
+        image: 'https://images.newindianexpress.com/uploads/user/imagelibrary/2020/4/8/w1200X800/Allu_Arjun.jpg'
+    },
+
+    {
+        id: 4,
+        name: 'Ntr',
+        age:35,
+        image: 'https://www.filmibeat.com/ph-big/2019/05/jr-ntr_1558329150210.jpg'
+    },
+
+    {
+        id: 5,
+        name: 'Prabash',
+        age:39,
+        image: 'https://pbs.twimg.com/profile_images/1428936441359323142/rdszNzrj_400x400.jpg'
+    }
+]
